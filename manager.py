4707a73ab4a8fe31ac1bd7fc9b23819913107cbd
Cal <cwoodruf@users.noreply.github.com>
2019-03-15 19:10:27 -0700

    clone of latest version on VR compuer

---
+++ manager.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python
+import re, json
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, render_template, make_response, jsonify, url_for
+from managerutils import *
+
+app = Flask('experiments')
+
+@app.route("/favicon.ico")
+def favicon():
+	return ""
+
+@app.route("/")
+def default():
+	"""
+	by default don't do anything
+	"""
+	return "CS Lab\n"
+
+@app.route("/api")
+@app.route("/help")
+def api_helper():
+	"""
+	list all the api calls and their arguments
+	"""
+	import urllib
+	output = []
+	seen = {}
+	for rule in app.url_map.iter_rules():
+		options = {}
+		for arg in rule.arguments:
+			options[arg] = "[{0}]".format(arg)
+		methods = ",".join(rule.methods)
+		url = url_for(rule.endpoint, **options)
+		line = urllib.unquote("{:20s} {}".format(methods, url))
+		if line in seen:
+			continue
+		seen[line] = True
+		output.append(line)
+
+	return "<pre>{0}</pre>".format("\n".join(sorted(output)))
+
+@app.route("/login/<key>/<nonce>")
+@app.route("/login/<key>",defaults={'nonce': None})
+def login(key, nonce):
+	resp = make_response()
+	tag = is_key_valid(key, request.environ, nonce)
+	if tag is None or not tag:
+		resp.set_data("ERROR: invalid key\n")
+	else:
+		result = save_auth(tag, request, resp)
+		if not result:
+			resp.set_data("ERROR: could not finish login\n")
+		else:
+			resp.set_data(result)
+	return resp
+
+@app.route("/new")
+def new_part():
+	print "checking login"
+	tag = check_auth_params(request)
+	print "tag", tag
+	if tag == False:
+		return "ERROR: not logged in\n";
+	ip = addr(request)
+	resp = make_response(new_participant(tag, ip))
+	return resp
+
+@app.route("/new/<key>/<nonce>")
+@app.route("/new/<key>",defaults={'nonce': None})
+def start(key, nonce):
+	"""
+	if we provide the right key we will get a new participant id
+	"""
+	ip = addr(request)
+	tag = check_auth_params(request)
+	if not tag:
+		tag = is_key_valid(key, request.environ, nonce)
+		if not tag:
+			return "ERROR: invalid key\n"
+		resp = make_response(new_participant(tag, ip))
+		if not save_auth(tag, request, resp):
+			resp.set_data("ERROR: login save failed\n")
+	else:
+		resp = make_response(new_participant(tag, ip))
+	return resp
+
+@app.route("/test")
+def test():
+	"""
+	check to see if we are logged in
+	"""
+	tag = check_auth_params(request)
+	if not tag:
+		return "not logged in"
+	return "tag {0} auth cookie {1}".format(tag, request.cookies["auth"])
+
+@app.route("/last")
+def last_participant():
+	"""
+	tell us the participant id of the last participant
+	also tells us the globally last participant
+	"""
+	tag = check_auth_params(request)
+	if not tag:
+		return "ERROR: not logged in\n";
+	ip = addr(request)
+	try:
+		with open(last_part_filename(tag), "r") as lh:
+			last_part = lh.read()
+		with open(running_filename(ip), "r") as rh:
+			running_part = rh.read()
+	except:
+		return "ERROR reading last part file\n"
+	return "last participant for {0}: {1}, global last: {2}\n".format(
+			ip, running_part, last_part)
+ 	
+@app.route("/first")
+def first_participant():
+	"""
+	figure out what the first participant should be for this experiment
+	"""
+	tag = check_auth_params(request)
+	first_part = -1
+	if not tag: 
+		return "ERROR: not logged in\n";
+	try:
+		with open(first_part_filename(tag), "r") as fh:
+			first_part_str = fh.read();
+			first_part = int(first_part_str)
+	except Exception as e:
+		return "ERROR reading first part file\n" 
+	return str(first_part)
+
+@app.route("/save/<part>", methods=["POST","PUT"])
+def save_data(part):
+	"""
+	saves data for a participant
+	note we don't do any data checking
+	example test:
+	curl -c cookies -b cookies -H 'content-type: text/plain' -XPOST http://localhost:13524/save/1234 -d "hi"
+	"""
+	try:
+		testpart = int(part)
+	except:
+		raise ValueError("participant id was not an integer")
+
+	if not check_auth_params(request):
+		return "ERROR: not logged in\n"
+
+	if request.headers['Content-Type'] == 'text/plain':
+		ip = addr(request)
+		data_file = data_filename(ip, part)
+		with open(data_file, "a+") as dh:
+			dh.write(request.data)
+		return "OK: saved {0} bytes to {1}\n".format(len(request.data), data_file)
+
+	return "ERROR: content-type should be text/plain not {0}\n".format(request.headers['Content-Type'])
+
+amap = {}
+@app.route("/arrangements/<dims>")
+def print_arrangements(dims):
+	"""
+	this generates a json array of arrays 
+	with the groups of stimuli for each trial
+	we want to have each way of presenting 
+	the color/shape position arrangements
+	be unique to only 1 or 2 participants
+	"""
+	if not check_auth_params(request):
+		return jsonify({"ERROR": "not logged in"})
+
+	if not re.match('^\d+x\d+\w*$', dims):
+		return jsonify({"ERROR": "bad dimensions"})
+
+	global amap
+	dimsfile = "tools/{0}.json".format(dims)
+	if not os.path.isfile(dimsfile):
+		return jsonify({"ERROR": "missing dim data"})
+
+	try:
+		import tools.mapper as m
+		if dimsfile in amap:
+			arrangements = amap[dimsfile]
+		else:
+			m.arrange_from_file(dimsfile)
+			amap[dimsfile] = m.arrangements
+	except Exception as e:
+		return jsonify({"ERROR": e})
+
+	return jsonify(m.arrangements.values())
+
+@app.route("/arrangementdims")
+def arrangement_files():
+	"""
+	prints a list of arrangement files
+	that can be used by the /arrangements/<dims> call
+	"""
+	if not check_auth_params(request):
+		return jsonify({"ERROR": "not logged in"})
+
+	dims = []
+	for (dirpath, dirnames, filenames) in os.walk("tools"):
+		for filename in filenames:
+			m = re.match("^(\d+x\d+)\.json", filename)
+			if m is None: continue
+			dims.append(m.group(1))
+
+	return jsonify(dims)
+
+@app.route("/arrangementdims/<dims>")
+def print_arrangementdims(dims):
+	"""
+	dumps one of the dimensions files used by /arrangements/<dims>
+	"""
+	if not check_auth_params(request):
+		return jsonify({"ERROR": "not logged in"})
+
+	if not re.match('^\d+x\d+\w*$', dims):
+		return jsonify({"ERROR": "bad dimensions"})
+
+	dimsfile = "tools/{0}.json".format(dims)
+	if not os.path.isfile(dimsfile):
+		return jsonify({"ERROR": "missing dim data"})
+
+	try:
+		with open(dimsfile, "r") as df:
+			return jsonify(json.load(df))
+	except Exception as e:
+		return jsonify({"ERROR": "cannot read file: {0}".format(e)}) 	
+
+if __name__ == '__main__':
+	try:
+		os.remove(part_lock_filename())
+	except:
+		pass
+
+	# for the debug server uncomment this
+	# app.run(debug=True,host='0.0.0.0',port=13524)
+
+	# the following can handle much heavier traffic
+	ssl_args={
+		'keyfile':os.path.join(appdir, 'certs', 'MyKey.key'), 
+		'certfile':os.path.join(appdir, 'certs', 'MyCertificate.crt')
+		#'keyfile':'/Users/collector/certbot/config/live/cslab.psyc.sfu.ca/privkey.pem',
+		#'certfile':'/Users/collector/certbot/config/live/cslab.psyc.sfu.ca/fullchain.pem'
+	}
+	use_tls = True
+	for k in ['keyfile','certfile']:
+		if os.path.isfile(ssl_args[k]):
+			print ssl_args[k],"exists"
+		else:
+			use_tls = False
+			break
+	if use_tls:
+		print "using TLS"
+		http_server = WSGIServer(('0.0.0.0', 13524), app, **ssl_args)
+	else:
+		print "running without TLS"
+		http_server = WSGIServer(('0.0.0.0', 13524), app)
+	http_server.serve_forever()
+
---
+++ managerutils.py
@@ -0,0 +1,318 @@
+"""
+The following functions help us manage who gets access to the system
+generating new participant ids (perhaps the hard way).
+The authentication process is that the external host runs
+
+ /login/<key> or /login/<hashed key>/<nonce>
+
+where key is a string that is stored for that IP a file appdir/keys
+which consists of:
+
+ <key> <tag> <ip address>
+
+lines. If the key exists in the file for the IP address we make a cookie
+with a random identifier that is used subsequently to authenticate the session.
+The cookies are stored in appdir/auth as files with 
+
+ <IP>-<auth_cookie>
+
+as their name. A cron task should remove these files periodically.
+
+When /new is run a new participant id is made for the given tag. 
+This id is just a monotonically increasing integer that is stored in 3 files. 
+Firstly, the last participant is stored in the last-participant.txt file. 
+We use this number to create the next participant id by incrementing it and 
+storing it again in the file. We also keep a list of which participant 
+was created for which host in participants.csv. Lastly we keep
+the last participant run by this host in running/<IP>.
+
+Change the last-participant.txt file to start participant numbers at a specific value.
+
+"""
+from base64 import b64encode
+from glob import glob
+import os, sys, random, time, string, hashlib, urllib
+
+debug = False;
+
+# sets up our application dir via environment variable
+# we cannot assume that os.getcwd() will be sensible
+# if this is run through an application server
+if 'EXPERIMENTS_DIR' not in os.environ:
+	appdir = os.getcwd()
+else:
+	appdir = os.environ['EXPERIMENTS_DIR']
+
+if not os.path.isdir(appdir):
+	raise ValueError("your appdir " + appdir + " is not a directory!")
+
+for sub in ["participants","auth","data",os.path.join("data","ips")]:
+	try:
+		os.makedirs(os.path.join(appdir, sub))
+	except Exception as e:
+		if not os.path.isdir(os.path.join(appdir, sub)):
+			raise Exception("ERROR: can't create subdirectory {0}: {1}".format(sub,e))
+
+nonce_seen = {}
+
+def key_filename():
+	return os.path.join(appdir,"keys")
+
+def read_keys():
+	"""
+	read the keys file - will have to restart to add keys
+	"""
+	keys = {}
+	with open(key_filename(), "r") as kh:
+		for l in kh:
+			l = l.strip()
+			if l.startswith("#"): continue
+			try:
+				key, tag, ip = l.split(" ")
+				keys[ip] = { "key": key, "tag": tag }
+			except:
+				print "could not read:", l
+	return keys
+		
+keys = read_keys()
+
+def addr(request):
+	if 'REMOTE_ADDR' in request.environ:
+		return request.environ['REMOTE_ADDR']
+
+def valid_nonce(nonce):
+	"""
+	validates the nonce - in this case whether we saw it 
+	we don't keep any evidence between runs of the server
+	"""
+	if nonce is None: return True
+	global nonce_seen
+	if nonce in nonce_seen:
+		 return False
+	nonce_seen[nonce] = True
+	if debug: print "nonce ok"
+	return True
+
+def remove_nonce(nonce):
+	"""
+	removes a nonce so we don't exhaust memory with bad requests etc.
+	"""
+	global nonce_seen
+	if nonce in nonce_seen:
+		del(nonce_seen[nonce])
+
+def create_hash(key, nonce, hashlen):
+	"""
+	makes a hash using a nonce
+	in our case these are pretty simple: hash(nonce + key)
+	"""
+	if nonce is None: return key
+	noncekey = "{0}{1}".format(nonce, key)
+	if hashlen == 32:
+		return hashlib.md5(noncekey).hexdigest()
+	elif hashlen == 40:
+		return hashlib.sha1(noncekey).hexdigest()
+	elif hashlen == 56:
+		return hashlib.sha224(noncekey).hexdigest()
+	elif hashlen == 64:
+		return hashlib.sha256(noncekey).hexdigest()
+	elif hashlen == 128:
+		return hashlib.sha512(noncekey).hexdigest()
+	else: 
+		return None
+
+def is_key_valid(key, environ, nonce=None):
+	"""
+	for logging in: checks the key file for our key
+	if we are using a hashed key use create_hash to 
+	build a hash for comparison
+	"""
+	key_file = key_filename()
+	if not os.path.isfile(key_file):
+		return False
+	
+	ip = environ['REMOTE_ADDR']
+
+	if ip not in keys: return False
+	if not valid_nonce(nonce): return False
+	ktest = create_hash(keys[ip]['key'], nonce, len(key))
+	if ktest is not None and ktest == key and 'tag' in keys[ip]:
+		return keys[ip]['tag']
+
+	remove_nonce(nonce)
+	return False
+
+def auth_key_filename(ip, auth_key):
+	"""
+	where we actually store the authentication key
+	typically the key is the file name
+	the file will contain some useful metadata if anything
+	"""
+	authdir = os.path.join(appdir,"auth")
+	return os.path.join(authdir,ip+"-"+urllib.quote_plus(auth_key)), os.path.join(authdir,ip+"*")
+
+def create_auth_params(tag, ip):
+	"""
+	creates a random session key used for a study run
+	this is a file that contains some useful session data
+	"""
+	auth_key = b64encode("".join([random.choice(string.printable) for _ in range(32)]))
+	auth_key_file, auth_pat = auth_key_filename(ip, auth_key)
+	for a in glob(auth_pat):
+		try:
+			os.unlink(a)
+		except Exception as e:
+			print "cannot unlink",a,"because of",e
+	with open(auth_key_file, "w+") as ah:
+		ah.write(tag)
+	return auth_key, auth_key_file
+
+def check_auth_params(req):
+	"""
+	search our cookies for the random session key
+	look for the file and read any metadata in the file
+	"""
+	ip = addr(req)
+	# sys.stdout.write("checking {0}\n".format(req.cookies))
+	if "auth" not in req.cookies: return False
+	auth_key = req.cookies["auth"]
+	auth_key_file, auth_pat = auth_key_filename(ip, auth_key)
+	# we should check the age of the authorization but removing the files would also work
+	if os.path.isfile(auth_key_file):
+		with open(auth_key_file, "r") as ah:
+			tag = ah.read()
+			return tag
+	return False
+
+def save_auth(tag, req, resp):
+	"""
+	creates an authentication file
+	also sets the cookie with the session key
+	"""
+	ip = addr(req)
+	auth_key, auth_key_file = create_auth_params(tag, ip)
+	try:
+		resp.set_cookie("auth", auth_key)
+		return auth_key
+	except:
+		resp.set_cookie("auth","",expires=0)
+	return False
+
+def part_dir(tag):
+	"""
+	base directory for saving data for experiments
+	"""
+	d = os.path.join(appdir, "participants", tag)
+	if os.path.isfile(d):
+		raise Exception("{0}: should be a directory!".format(d))
+	if not os.path.isdir(d):
+		os.makedirs(d)
+	return d
+
+def part_lock_filename(tag):
+	"""
+	lock file for creating a new participant id
+	if this does not get deleted between uses it will
+	block anyone else making a new participant id
+	"""
+	return os.path.join(part_dir(tag), "participants.csv.lock")
+
+def first_part_filename(tag):
+	"""
+	this file tracks the first participant number
+	if we want to arbitrarily start numbering participants
+	we can set the number in this file
+	"""
+	return os.path.join(part_dir(tag), "first-participant.txt")
+
+def last_part_filename(tag):
+	"""
+	this file tracks the last participant number
+	if we want to arbitrarily start numbering participants
+	we can set the number in this file
+	"""
+	return os.path.join(part_dir(tag), "last-participant.txt")
+
+def part_filename(tag):
+	"""
+	a list of which participants actually used which host
+	"""
+	return os.path.join(part_dir(tag), "participants.csv")
+
+def data_dir(tag, ip):
+	data_dir = os.path.join(part_dir(tag), "data", ip)
+	if os.path.isfile(data_dir):
+		raise Exception("data dir {0} is a file!".format(data_dir))
+	if not os.path.isdir(data_dir):
+		os.makedirs(data_dir)
+	return data_dir
+	
+def last_data_filename(ip, tag):
+	"""
+	record of the last data file for a given ip address
+	"""
+	return os.path.join(data_dir(tag, ip), "{0}-latest.txt".format(ip))
+
+def data_filename(ip, part):
+	"""
+	create a file name for a data file
+	"""
+	if ip not in keys or 'tag' not in keys[ip]:
+		raise Exception("missing data on ip {0}".format(ip))
+
+	tag = keys[ip]['tag']
+	filename = "{0}-{1}.txt".format(ip, part)
+	ddir = data_dir(tag, ip)
+	fullpath = os.path.join(ddir, filename)
+
+	with open(last_data_filename(ip, tag), "w+") as df:
+		df.write(fullpath)
+
+	return fullpath
+
+def new_participant(tag, ip):
+	"""
+	gets a new participant id in a way where we can have multiple hosts doing the same thing
+	at the same time
+	since file locking doesn't work on mac os 
+	http://chris.improbable.org/2010/12/16/everything-you-never-wanted-to-know-about-file-locking/
+	using a lock file to control access
+	"""
+	part_file = part_filename(tag)
+	last_part_file = last_part_filename(tag)
+	lock_file = part_lock_filename(tag)
+	slept = 1
+	lock_key = "{0} {1}".format(ip, random.random())
+	while os.path.isfile(lock_file):
+		time.sleep(slept * 0.2)
+		slept += 1
+		if slept >= 20:
+			return "TIMED OUT\n"
+
+	with open(lock_file, "w+") as lh:
+		lh.write(lock_key)
+
+	with open(lock_file, "r") as lh:
+		testkey = lh.read()
+		if testkey != lock_key:
+			return "TRY AGAIN\n"
+
+	try:
+		last_part = 0
+		with open(last_part_file, "r") as ph:
+			last_part = int(ph.read())
+		os.remove(last_part_file)
+		last_part += 1
+	except:
+		last_part = 1 # was: int(time.time() * 1000.0)
+
+	with open(last_part_file, "w+") as ph:
+		ph.write(str(last_part))
+
+	with open(part_file, "a+") as ph:
+		ph.write("{0}, {1}\n".format(last_part, ip))
+
+	if os.path.isfile(lock_file): 
+		os.remove(lock_file)
+	return str(last_part)
+
